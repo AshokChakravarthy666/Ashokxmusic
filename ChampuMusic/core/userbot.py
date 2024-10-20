cfe9dd0057fddcb59496acf469c24240d4b8681d
𝙰𝚂𝙷𝙾𝙺𝚇𝙶𝙷𝙾𝚂𝚃 <106798009+AshokChakravarthy666@users.noreply.github.com>
2024-10-21 00:26:35 +0530

    Update userbot.py

--- ChampuMusic/core/userbot.py
+++ ChampuMusic/core/userbot.py
@@ -63,10 +63,11 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("https://t.me/+HMHH1y96Sz1jMmVl")
-                await self.one.join_chat("https://t.me/+ITKJ6qtpM0hiOGU9")
-                await self.one.join_chat("https://t.me/+5adkfK7wdAswMjNl")
-
+                await self.one.join_chat("TheShivanshu")
+                await self.one.join_chat("TheChampuClub")
+                await self.one.join_chat("akaChampu")
+                await self.one.join_chat("TheChampu")
+                
             except:
                 pass
             assistants.append(1)
@@ -92,9 +93,11 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("https://t.me/+HMHH1y96Sz1jMmVl")
-                await self.two.join_chat("https://t.me/+ITKJ6qtpM0hiOGU9")
-                await self.two.join_chat("https://t.me/+5adkfK7wdAswMjNl")
+                await self.two.join_chat("TheShivanshu")
+                await self.two.join_chat("TheChampuClub")
+                await self.two.join_chat("akaChampu")
+                await self.two.join_chat("TheChampu")
+                
             except:
                 pass
             assistants.append(2)
@@ -115,9 +118,11 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("https://t.me/+HMHH1y96Sz1jMmVl")
-                await self.three.join_chat("https://t.me/+ITKJ6qtpM0hiOGU9")
-                await self.three.join_chat("https://t.me/+5adkfK7wdAswMjNl")
+                await self.three.join_chat("TheShivanshu")
+                await self.three.join_chat("TheChampuClub")
+                await self.three.join_chat("akaChampu")
+                await self.three.join_chat("TheChampu")
+                
             except:
                 pass
             assistants.append(3)
@@ -139,9 +144,10 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("https://t.me/+HMHH1y96Sz1jMmVl")
-                await self.four.join_chat("https://t.me/+ITKJ6qtpM0hiOGU9")
-                await self.four.join_chat("https://t.me/+5adkfK7wdAswMjNl")
+                await self.four.join_chat("TheShivanshu")
+                await self.four.join_chat("TheChampuClub")
+                await self.four.join_chat("akaChampu")
+                await self.four.join_chat("TheChampu")
             except:
                 pass
             assistants.append(4)
@@ -163,10 +169,10 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("https://t.me/+HMHH1y96Sz1jMmVl")
-                await self.five.join_chat("https://t.me/+ITKJ6qtpM0hiOGU9")
-                await self.five.join_chat("https://t.me/+5adkfK7wdAswMjNl")
-                
+                await self.five.join_chat("TheShivanshu")
+                await self.five.join_chat("TheChampuClub")
+                await self.five.join_chat("akaChampu")
+                await self.five.join_chat("TheChampu")
             except:
                 pass
             assistants.append(5)
